42914f74a832e916b0df45cf1bee5dfdb95b11c9
Hajin-Bang <hajin0619@naver.com>
2024-10-17 13:50:29 +0900

    [Feat]-페이지 라우팅 기본 설계

---
+++ src/components/pages/CheckoutPage.tsx
@@ -0,0 +1,9 @@
+const CheckoutPage = () => {
+  return (
+    <div>
+      <h1>결제 페이지</h1>
+    </div>
+  );
+};
+
+export default CheckoutPage;
---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const BooksListPage = () => {
+  return (
+    <div>
+      <h1>도서 탐색 메인 페이지</h1>
+    </div>
+  );
+};
+
+export default BooksListPage;
