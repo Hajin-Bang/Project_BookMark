8983b7d65bb281b7db2d217c50c8c9f9bd5e380a
Hajin-Bang <hajin0619@naver.com>
2024-10-21 17:38:58 +0900

    [Refactory]-소셜 로그인 확장 가능한 구조로 수정

--- src/components/common/components/GoogleSignIn.tsx
+++ src/components/common/components/GoogleSignIn.tsx
@@ -6,30 +6,43 @@ import { doc, getDoc } from "firebase/firestore";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 
+// 공통 로그인 처리
+const handleSocialLogin = async (provider: GoogleAuthProvider) => {
+  const auth = getAuth();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential?.accessToken;
+    const user = result.user;
+
+    const userDoc = await getDoc(doc(db, "users", user.uid));
+    if (!userDoc.exists()) {
+      throw new Error("유저 정보를 찾을 수 없습니다.");
+    }
+    const userData = userDoc.data();
+
+    if (token) {
+      Cookies.set("accessToken", token, { expires: 7 });
+    } else {
+      console.error("토큰을 가져오지 못했습니다.");
+    }
+
+    return { user, userData };
+  } catch (error) {
+    console.error(`${provider.providerId} 로그인 실패`, error);
+    throw error;
+  }
+};
+
+// 구글 로그인 처리
 export const GoogleSignIn = () => {
   const navigate = useNavigate();
   const { setUser, setIsLogin } = useAuthStore();
 
   const handleGoogleSignIn = async () => {
-    const auth = getAuth();
     const provider = new GoogleAuthProvider();
     try {
-      const result = await signInWithPopup(auth, provider);
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential?.accessToken;
-      const user = result.user;
-
-      const userDoc = await getDoc(doc(db, "users", user.uid));
-      if (!userDoc.exists()) {
-        throw new Error("유저 정보를 찾을 수 없습니다.");
-      }
-      const userData = userDoc.data();
-
-      if (token) {
-        Cookies.set("accessToken", token, { expires: 7 });
-      } else {
-        console.error("토큰을 가져오지 못했습니다.");
-      }
+      const { user, userData } = await handleSocialLogin(provider);
 
       setUser({
         uid: user.uid,
@@ -45,7 +58,7 @@ export const GoogleSignIn = () => {
         navigate("/");
       }
     } catch (error) {
-      console.error("Google 로그인 실패", error);
+      console.error("Google 로그인 중 에러 발생", error);
     }
   };
 
